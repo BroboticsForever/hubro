ef7d828ec789ecc70328ad2cd4fb094c8bc7e527
Ian Tait <thetaiter@ku.edu>
2014-09-20 14:15:58 -0500

    Added jesus feature

---
+++ hubro/bin/start_local_hubro.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export HUBOT_HIPCHAT_JID="166093_1205621@chat.hipchat.com"
+export HUBOT_HIPCHAT_PASSWORD="11aaAAaa"
+export HUBOT_HIPCHAT_ROOMS="166093_hubros_house@conf.hipchat.com,166093_hubro_test@conf.hipchat.com"
+export HUBOT_HIPCHAT_JOIN_ROOMS_ON_INVITE=false
+
+bin/hubot -a hipchat
