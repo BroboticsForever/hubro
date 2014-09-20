68db3333cde863cb5599f81c14866590ea513f09
Ian Tait <thetaiter@ku.edu>
2014-09-20 15:18:47 -0500

    hubro-update deployment attempt

--- hubro/bin/jesus.sh
+++ hubro/bin/jesus.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-export HUBRO_HOME=/home/thetaiter/Development/git/hubro
-
-echo "Killing node..." >> /home/thetaiter/Development/logs/hubro.log 2>&1
+echo "Killing node..." >> /var/log/hubro.log 2>&1
 killall node
-echo "Sleeping for 3 minutes..." >> /home/thetaiter/Development/logs/hubro.log 2>&1
+echo "Sleeping for 3 minutes..." >> /var/log/hubro.log 2>&1
 sleep 180
-echo "Resurrecting Hubro..." >> /home/thetaiter/Development/logs/hubro.log 2>&1
-nohup ${HUBRO_HOME}/hubro/bin/start_local_hubro.sh >> /home/thetaiter/Development/logs/hubro.log 2>&1 &
+echo "Resurrecting Hubro..." >> /var/log/hubro.log 2>&1
+nohup /etc/my_init.d/start_hubro.sh >> /var/log/hubro.log 2>&1 &
