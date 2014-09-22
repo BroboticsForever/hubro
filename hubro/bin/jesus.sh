4c1b49ffef6595d70eab36138be3f2a98ad29449
Ian Tait <thetaiter@ku.edu>
2014-09-21 21:20:57 -0500

    Changed death time to 3 minutes

--- hubro/bin/jesus.sh
+++ hubro/bin/jesus.sh
@@ -9,7 +9,7 @@ fi
 sleep 1
 echo "Killing node..." >> /var/log/hubro.log 2>&1
 killall node
-echo "Sleeping for 3 minutes..." >> /var/log/hubro.log 2>&1
-sleep 180
+echo "Sleeping for 3 mginutes..." >> /var/log/hubro.log 2>&1
+sleep 179
 echo "Resurrecting Hubro..." >> /var/log/hubro.log 2>&1
 nohup ${START_HUBRO} >> /var/log/hubro.log 2>&1 &
